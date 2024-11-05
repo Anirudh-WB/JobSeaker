5e1dbdd69730c3f3f4537e5c5464af7a97a3c835
Anirudh Gupta <anirudh.gupta@wonderbiz.in>
2024-11-05 11:07:55 +0530

    react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import ProfilePage from "./components/ProfilePage/ProfilePage";
 import HomePage from "./components/HomePage/HomePage";
+import Navbar from "./components/NavBar/Navbar";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App bg-gray-50 h-screen w-screen overflow-auto flex flex-col justify-center items-center">
-      <HomePage/>
-      <ProfilePage />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
     </div>
   );
 }
