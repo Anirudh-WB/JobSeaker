7f9ca0fb124133f18a4cf6615f96127cb757e4f1
Anirudh Gupta <anirudh.gupta@wonderbiz.in>
2024-11-05 10:54:05 +0530

    profile header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import ProfilePage from "./components/ProfilePage/ProfilePage";
+
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-gray-50 h-screen w-screen overflow-auto flex flex-col justify-center items-center">
       <h1>Hello</h1>
+      <ProfilePage />
     </div>
   );
 }
--- src/components/ProfilePage/ProfilePage.jsx
+++ src/components/ProfilePage/ProfilePage.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import ProfileHeader from "./ProfileHeader";
 
 function ProfilePage() {
-  return <div>ProfilePage</div>;
+  return (
+    <div className="py-8 px-48 w-full">
+      <ProfileHeader />
+    </div>
+  );
 }
 
 export default ProfilePage;
