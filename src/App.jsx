405c1947b2d16d7b7dc3cb8fb3403dbf10029496
Nimesh Jethva <nimesh.jethva@wonderbiz.in>
2024-11-06 14:12:38 +0530

    Login added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
-import ProfilePage from "./components/ProfilePage/ProfilePage";
-import HomePage from "./components/HomePage/HomePage";
+import React, { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/NavBar/Navbar";
 import Joblist from "./components/JobList/Joblist";
 import JobDetails from "./components/JobDetails/JobDetails";
+import ProfilePage from "./components/ProfilePage/ProfilePage";
+import HomePage from "./components/HomePage/HomePage";
+import LoginSidebar from "./components/SideBar/LoginSideBar";
 
 function App() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
     <div className="App bg-gray-50 h-screen w-screen flex flex-col">
-      <Navbar />
-      <div className="overflow-auto h-full">
+      <Navbar toggleSidebar={toggleSidebar} />
+      <LoginSidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+      <div className="h-full pt-10">
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/profile" element={<ProfilePage />} />
           <Route path="/job-list" element={<Joblist />} />
-          <Route path="/job-details" element={<JobDetails/> }/>
+          <Route path="/job-details" element={<JobDetails />} />
         </Routes>
       </div>
     </div>
