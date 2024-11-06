adadc84d4e0e36e410ae6806109b555dbe34bcc8
Anirudh Gupta <anirudh.gupta@wonderbiz.in>
2024-11-06 12:50:34 +0530

    key skills and resume headline modals

--- src/components/ProfilePage/KeySkills.jsx
+++ src/components/ProfilePage/KeySkills.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
+import { FiEdit2 } from "react-icons/fi";
+import KeySkillsModal from "./KeySkillsModal";
 
 function KeySkills() {
-  const skills = [
+  let [isKeySkillsOpen, setIsKeySkillsOpen] = useState(false);
+  let [skills, setSkills] = useState([
     "C #",
     "Asp.Net Core",
     ".Net Core",
@@ -13,21 +16,35 @@ function KeySkills() {
     "Ado.net",
     "React Js",
     "Jquery",
-  ];
+  ]);
 
   return (
-    <div className="p-5 bg-white rounded-xl shadow-md h-fit flex flex-col gap-4">
-      <h2 className="font-semibold text-md" id="Key-skills">
-        Key skills
-      </h2>
-      <div className="flex items-center gap-1 flex-wrap text-sm text-gray-700">
-        {skills.map((skill, index) => (
-          <span className="border rounded-full px-3 py-1" key={index}>
-            {skill}
-          </span>
-        ))}
+    <>
+      <div className="p-5 bg-white rounded-xl shadow-md h-fit flex flex-col gap-4">
+        <div className="flex items-center gap-4">
+          <h2 className="font-semibold text-md" id="Key-skills">
+            Key skills
+          </h2>
+          <button onClick={() => setIsKeySkillsOpen((prev) => !prev)}>
+            <FiEdit2 className="text-sm text-gray-700" />
+          </button>
+        </div>
+        <div className="flex items-center gap-1 flex-wrap text-sm text-gray-700">
+          {skills.map((skill, index) => (
+            <span className="border rounded-full px-3 py-1" key={index}>
+              {skill}
+            </span>
+          ))}
+        </div>
       </div>
-    </div>
+
+      <KeySkillsModal
+        isOpen={isKeySkillsOpen}
+        setIsOpen={setIsKeySkillsOpen}
+        skills={skills}
+        setSkills={setSkills}
+      />
+    </>
   );
 }
 
