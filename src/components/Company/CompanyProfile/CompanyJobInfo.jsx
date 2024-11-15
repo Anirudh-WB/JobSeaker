aed0f876efb2378c36954c7b71e6738062b8c9cf
Nimesh Jethva <nimesh.jethva@wonderbiz.in>
2024-11-15 15:29:02 +0530

    content added

--- src/components/Company/CompanyProfile/CompanyJobInfo.jsx
+++ src/components/Company/CompanyProfile/CompanyJobInfo.jsx
@@ -4,25 +4,27 @@ import boyPic from "../../../assets/white-boy.a0d2814a.png";
 
 function CompanyJobInfo() {
   return (
-    <div className="bg-white border rounded-lg p-7 flex flex-col items-center gap-5 w-1/4 sticky top-10 h-fit">
+    <div className="bg-white border rounded-lg p-7 flex flex-col items-center gap-5 w-1/4 sticky top-0 h-fit">
       <img src={boyPic} alt="" className=" w-1/2" />
-      <h2 className="font-semibold">On registering, you can</h2>
+      <h2 className="font-semibold">On Job Posting, you can</h2>
       <div className="flex gap-1 w-full font-medium">
-        <GoCheckCircleFill className="text-green-500 w-1/12 mt-1" />
+        <GoCheckCircleFill className="text-green-500 w-1/6 mt-1" />
         <p className="text-gray-600 text-sm w-auto">
-          Build your own profile and let recruiters find you
+          Join our team and be part of exciting projects that push the
+          boundaries of innovation
         </p>
       </div>
       <div className="flex gap-1 w-full font-medium">
-        <GoCheckCircleFill className="text-green-500 w-1/12 mt-1" />
+        <GoCheckCircleFill className="text-green-500 w-1/6 mt-1" />
         <p className="text-gray-600 text-sm w-auto">
-          Get job postings delivered right to your email
+          We’re looking for skilled professionals with a passion for technology
+          and growth.
         </p>
       </div>
       <div className="flex gap-1 w-full font-medium">
-        <GoCheckCircleFill className="text-green-500 w-1/12 mt-1" />
+        <GoCheckCircleFill className="text-green-500 w-1/6 mt-1" />
         <p className="text-gray-600 text-sm w-auto">
-          Find a job and grow your career
+          Take the next step in your career with us and help shape the future.
         </p>
       </div>
       <button className="text-white bg-blue-600 py-1.5 px-3 rounded-full">
